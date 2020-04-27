a6188ef868310517136583c35ef02c4da1ce44dc
Natalia Mirnaia <strunailustra@gmail.com>
2020-04-27 13:18:44 +0300

    Initial Commit

---
+++ HarryPotterAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HarryPotterAPI
+//
+//  Created by Наталья Мирная on 27.04.2020.
+//  Copyright © 2020 Наталья Мирная. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
